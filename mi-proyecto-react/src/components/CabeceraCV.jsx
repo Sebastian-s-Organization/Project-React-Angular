5c9e6744559b731197198dd533c743e759cb979f
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-11-21 20:11:44 -0500

    chore: integrar componentes CV en App.jsx

---
+++ mi-proyecto-react/src/components/CabeceraCV.jsx
@@ -0,0 +1,14 @@
+// src/components/CabeceraCV.jsx
+import React from "react";
+
+export default function CabeceraCV() {
+  return (
+    <header style={{ background: "#007acc", color: "#fff", padding: "20px" }}>
+      <h1>Tu Nombre Completo</h1>
+      <p>
+        Desarrollador Front-End Jr. | Tel: 300-000-0000 | correo@ejemplo.com |
+        Ciudad
+      </p>
+    </header>
+  );
+}
