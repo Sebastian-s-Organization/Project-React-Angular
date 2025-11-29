b13ccc21ef71b99f1aee0f07f96f7871279db0d4
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-11-28 19:26:23 -0500

    feat: componente CabeceraCV ahora recibe props

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -2,16 +2,11 @@ import React from "react";
 
 export default function CabeceraCV({ nombre, cargo, ciudad, contacto }) {
   return (
-    <React.Fragment>
-      <header style={{ background: "#007acc", color: "#fff", padding: "20px" }}>
-        {" "}
-        <h1>{nombre || "Sebastian Castro Grajales"}</h1>{" "}
-        <p>
-          {cargo || "Análisis y desarrollo de Software"} |{" "}
-          {contacto || "[sebastiancastro4848@gmail.com)"} |{" "}
-          {ciudad || "Medellín, Colombia"}{" "}
-        </p>{" "}
-      </header>
-    </React.Fragment>
+    <header style={{ background: "#007acc", color: "#fff", padding: "20px" }}>
+      <h1>{nombre}</h1>
+      <p>
+        {cargo} | {contacto} | {ciudad}
+      </p>
+    </header>
   );
 }
