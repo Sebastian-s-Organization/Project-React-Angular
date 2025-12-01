466882d5497f306b646e5de73787fd7332f6f352
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-12-01 15:06:33 -0500

    feat: crear componente Projects con lista de proyectos

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+function Projects() {
+  return (
+    <section>
+      <h2>Proyectos</h2>
+      <ul>
+        <li>Proyecto 1: Aplicación de tareas en React</li>
+        <li>Proyecto 2: Página web con HTML y CSS</li>
+        <li>Proyecto 3: API con Node.js y Express</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
