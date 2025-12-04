60d2818e1c842cb10001da61f6417737bfab59b3
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-12-03 22:57:42 -0500

    feat: creación de la ruta usuarios con su vista correspondiente

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,20 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Todos from "./pages/Todos";
 import Registro from "./pages/Registro";
-import Footer from "./components/Footer";
+import Usuarios from "./pages/Usuarios"; // ⬅️ NUEVO
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todos" element={<Todos />} />
-          <Route path="/registro" element={<Registro />} />
-        </Routes>
-
-        <Footer />
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/usuarios" element={<Usuarios />} /> {/* ⬅️ NUEVO */}
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ todo-app/src/pages/Usuarios.jsx
@@ -0,0 +1,10 @@
+function Usuarios() {
+  return (
+    <div>
+      <h1>Página de Usuarios</h1>
+      <p>Aquí se mostrarán los usuarios.</p>
+    </div>
+  );
+}
+
+export default Usuarios;
