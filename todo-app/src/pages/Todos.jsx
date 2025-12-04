af772e90315affeb347485919283207f07fe5544
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-12-03 20:36:54 -0500

    feat: componente Todos.jsx con fetch inicial usando useEffect

--- todo-app/src/pages/Todos.jsx
+++ todo-app/src/pages/Todos.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+
 function Todos() {
-  return <h1>Página de Todos</h1>;
+  useEffect(() => {
+    // Fetch inicial según la guía
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Datos recibidos desde API:", data);
+      })
+      .catch((err) => {
+        console.error("Error al obtener los datos:", err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Página Todos</h1>
+      <p>Realizando fetch inicial... revisa la consola</p>
+    </div>
+  );
 }
 
 export default Todos;
