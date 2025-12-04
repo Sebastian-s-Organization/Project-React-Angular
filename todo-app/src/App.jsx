131b6282eedc2f110ee67b4c02e5c676d12ab321
Sebastian Castro <sebastiancastro4848@gmail.com>
2025-12-03 22:53:27 -0500

    feat: agregar componente Footer y mostrarlo en toda la aplicación

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Todos from "./pages/Todos";
 import Registro from "./pages/Registro";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="/registro" element={<Registro />} />
-      </Routes>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<Todos />} />
+          <Route path="/registro" element={<Registro />} />
+        </Routes>
+
+        <Footer />
+      </>
     </BrowserRouter>
   );
 }
---
+++ todo-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        marginTop: "40px",
+        opacity: 0.6,
+      }}
+    >
+      <p>© 2025 - Mi Proyecto React - Todos los derechos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
